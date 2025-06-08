defbb7e5ce5082d16fe8c4f9e2f73ae7467942d5
Ti33Y <timeti8160@gmail.com>
2025-06-08 10:51:26 +0700

    V.2 แก้ 01 เพิ่ม 07

--- lecture01.c
+++ lecture01.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 void main(){
     printf("Hello...");           
-
+    printf("HI...");
+    printf("Hey...");
 
 
 }
---
+++ lecture07.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+void main(){
+   printf("wellcome to IoT-SAU");
+
+}
