258d1b29f0fa1318db06bbbd28021867cb6621cb
Ti33Y <timeti8160@gmail.com>
2025-06-08 11:50:07 +0700

    V3แก้ 07

--- lecture07.c
+++ lecture07.c
@@ -1,5 +1,21 @@
 #include <stdio.h>
 void main(){
-   printf("wellcome to IoT-SAU");
+   int aa = 10000;   //(%d)
+   long ff =1999999; //(%ld)
+   float bb = 20.52567; //(%f)
+   double cc = 36.8555465245; //(%lf)
+   char dd = 't'; //(%c)
+   char ee[] = "timmy"; //(%s)
+
+   printf("\nAA : %d", aa);
+   printf("\nFF : %ld", ff);
+   printf("\nBB : %f", bb);
+   printf("\nCC : %lf", cc);
+   printf("\nDD : %c", dd);
+   printf("\nEE : %s", ee);
+
+
+
+
 
 }
